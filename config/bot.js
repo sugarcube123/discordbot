fd572cbaba7ca3a7c0ea007675c5c8c075f9d600
sugarcube123 <jonathancalvian.f@gmail.com>
2021-06-20 17:03:32 +0700

    cock
    ball

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODU1NzE3MDAyNzcyNzQyMTU0.YM2ijw.AnBSSSpgi1ANEWeYepgy_PEXyII',
+        prefix: ';',
+        activity: 'Cock and ball dungeoneer tycoon',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
